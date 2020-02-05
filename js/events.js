c258c97a1dc5f2b874ab3df82b959e94848d5d94
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2020-02-05 13:11:24 +0100

    fichiers php et requetes get initialisés

--- js/events.js
+++ js/events.js
@@ -4,12 +4,39 @@
 /* ***************************************** FONCTIONS ***************************************** */
 /*************************************************************************************************/
 
-function onClickExecute()
+function onClickExecute(event)
 {
-    var radioChoice;
+    let radioChoice;
+
+    event.preventDefault();
 
     // Récupération de la valeur du bouton radio qui a été sélectionné.
     radioChoice = $('input[name=what]:checked').val();
 
     alert(radioChoice);
+
+    //récupération des données à partir du choix de l'utilisateur 
+    if(radioChoice == "1") {
+    
+        $.get("../php/scriptImporterHTML.php", function(response) {
+            $("#target").html(response);
+
+        });
+    }else if(radioChoice == "2") {
+
+        $.get("../php/scriptImporterJSON.php", function(response) {
+            $("#target").html();
+        });
+
+    }else if(radioChoice == "3") {
+
+        $.get("../php/radioChoice.php", function(response) {
+            $("#target").html();
+        });
+    }
+
+    
+
+
+
 }
