9a8c90a4f5a4514a7190cae8e0db7f4f275e664a
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2020-02-05 12:28:04 +0100

    fichiers intiales ajoutés

--- js/events.js
+++ js/events.js
@@ -1,7 +1,15 @@
+'use strict';
 
+/*************************************************************************************************/
+/* ***************************************** FONCTIONS ***************************************** */
+/*************************************************************************************************/
 
+function onClickExecute()
+{
+    var radioChoice;
 
-function onClickSubmitButton() {
+    // Récupération de la valeur du bouton radio qui a été sélectionné.
+    radioChoice = $('input[name=what]:checked').val();
 
-  alert($("input:checked").val());
+    alert(radioChoice);
 }
